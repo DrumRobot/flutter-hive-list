1e6c739424ee23dc55ed337aa788838e4dd2b61f
David Jeong <drumrobot@1do.space>
2022-06-20 17:01:57 +0900

    build: add scrollable positioned list

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 
 void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox<int>('values');
   runApp(const MyApp());
 }
 
@@ -15,35 +19,41 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class MyHomePage extends StatelessWidget {
+  final itemController = ItemScrollController();
+
+  MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
   void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+    var values = Hive.box<int>('values');
+    var index = values.length - 1;
+    var last = index >= 0 ? values.getAt(index) ?? 0 : 0;
+    values.add(last + 1);
+    itemController.scrollTo(index: index, duration: const Duration(seconds: 1));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(title),
       ),
+      body: ValueListenableBuilder<Box<int>>(
+        valueListenable: Hive.box<int>('values').listenable(),
+        builder: (BuildContext context, Box<int> values, Widget? child) =>
+            ScrollablePositionedList.builder(
+          itemScrollController: itemController,
+          itemCount: values.length,
+          itemBuilder: (context, index) =>
+              buildItem(context, values.keyAt(index), values.getAt(index)!),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
@@ -52,4 +62,17 @@ class _MyHomePageState extends State<MyHomePage> {
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  Widget buildItem(context, int key, int value) => GestureDetector(
+        key: Key(key.toString()),
+        onDoubleTap: () {
+          Hive.box<int>('values').delete(key);
+        },
+        onTap: () {
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: Text('Double tap to delete $value'),
+          ));
+        },
+        child: ListTile(title: Text('Item $value')),
+      );
 }
